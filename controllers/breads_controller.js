7a5374cc19d79e01929e6747f23c9422372f0031
Ceddyp19 <ceddyp117@gmail.com>
2022-05-04 13:52:18 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -25,7 +25,8 @@ breads.get('/:arrayIndex', (req, res) => {
     // res.send(Bread[req.params.arrayIndex])
     if (Bread[req.params.arrayIndex]) {
         res.render('Show', {
-            bread: Bread[req.params.arrayIndex]
+            bread: Bread[req.params.arrayIndex],
+            index: req.params.arrayIndex
         })
     } else {
        // res.send('404')
@@ -45,10 +46,15 @@ breads.post('/', (req, res) => {
       req.body.hasGluten = false
     }
     Bread.push(req.body)
-    res.redirect('/breads')
+    res.status(303).redirect('/breads')
   })
   
-  
+  // DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
 
 
 module.exports = breads
