4e9f9563565a9aa84b88db4f3d7f56dd570d09d6
Ceddyp19 <ceddyp117@gmail.com>
2022-05-14 10:26:12 -0500

    added mongoose helper methods for delete and update routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,79 +1,123 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const seedData = require('../models/seed.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
+  Bread.find()
+    .then(foundBreads => {
+      res.render('Index',
         {
-            breads: Bread,
-            title: 'Index Page'
-        }
+          breads: foundBreads,
+          title: 'Index Page'
+        })
+    })
 
-    )
-    //res.send(Bread)
+  // )
+  //res.send(Bread)
 })
 
 // NEW
 breads.get('/new', (req, res) => {
-    res.render('new')
+  res.render('new')
 })
 
 // EDIT
 breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+  // res.render('edit', {
+  //   bread: Bread[req.params.indexArray],
+  //   index: req.params.indexArray
+  // })
+
+  Bread.findById(req.params.indexArray)
+    .then(foundBread => {
+      console.log('edit route', foundBread)
+      res.render('edit', {
+        bread: foundBread
+      })
+    })
 })
 
 
 // SHOW INDIVIUAL 
-breads.get('/:arrayIndex', (req, res) => {
-    // res.send(Bread[req.params.arrayIndex])
-    if (Bread[req.params.arrayIndex]) {
-        res.render('Show', {
-            bread: Bread[req.params.arrayIndex],
-            index: req.params.arrayIndex
-        })
-    } else {
-       // res.send('404')
-       res.render('404')
-    }
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
+    })
+    .catch(err => {
+      res.send('404')
+    })
+
+
+  // // res.send(Bread[req.params.arrayIndex])
+  // if (Bread[req.params.arrayIndex]) {
+  //   res.render('Show', {
+  //     bread: Bread[req.params.arrayIndex],
+  //     index: req.params.arrayIndex
+  //   })
+  // } else {
+  //   // res.send('404')
+  //   res.render('404')
+  // }
 
 })
 
 // CREATE
 breads.post('/', (req, res) => {
-    if (!req.body.image) {
-      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-    }
-    if(req.body.hasGluten === 'on') {
-      req.body.hasGluten = true
-    } else {
-      req.body.hasGluten = false
-    }
-    Bread.push(req.body)
-    res.status(303).redirect('/breads')
-  })
-  
-  // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
-})
 
-// UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
+  if (!req.body.image) {
+    req.body.image = 'https://images.unsplash.com/photo-1567042661848-7161ce446f85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnJlYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+  }
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+
+  Bread.create(req.body)
+  res.status(303).redirect('/breads')
 })
 
+// DELETE
+// breads.delete('/:indexArray', (req, res) => {
+//   Bread.splice(req.params.indexArray, 1)
+//   res.status(303).redirect('/breads')
+// })
+
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id)
+    .then(deletedBread => {
+      res.status(303).redirect('/breads')
+    })
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  // Bread[req.params.arrayIndex] = req.body
+  // res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.arrayIndex, req.body, { new: true })
+    .then(updatedBread => {
+      console.log(updatedBread)
+      res.redirect(`/breads/${req.params.arrayIndex}`)
+    })
+})
+
+//SEED DATA ROUTE
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany(seedData)
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
 
 module.exports = breads
 
--- views/show.jsx
+++ views/show.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 const Default = require('./layouts/Default')
 
 function Show({ bread, index }) {
-    console.log(bread.name)
+    // console.log(bread.name)
     return (
         <Default>
             <h2>Show Page</h2>
@@ -17,13 +17,12 @@ function Show({ bread, index }) {
             </p>
             <img src={bread.image} alt={bread.name} />
 
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
 
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
-
 
         </Default>
     )
